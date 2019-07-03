897434118ce9f8f8733030babc86aaaae55c1cbe
PurpleLover <sepan223@gmail.com>
2019-07-03 11:29:46 +0700

    Update ReactotronConfig.js
    Added support to run test on device

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -1,7 +1,13 @@
+import { NativeModules } from 'react-native';
 import Reactotron from 'reactotron-react-native';
 // import {reactotronRedux} from 'reactotron-redux';
+let scriptHostname;
+if (__DEV__) {
+  const scriptURL = NativeModules.SourceCode.scriptURL;
+  scriptHostname = scriptURL.split('://')[1].split(':')[0];
+}
 Reactotron
-  .configure() // controls connection & communication settings
+  .configure({ host: scriptHostname }) // controls connection & communication settings
   .useReactNative() // add all built-in react native plugins
   // .use(reactotronRedux()) // subscription to redux
   .connect() // let's connect!
