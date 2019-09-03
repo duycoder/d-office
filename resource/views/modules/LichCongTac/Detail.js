efe0133840dfc3d96a8ddb639dd8d4b630d8a9ce
PurpleLover <sepan223@gmail.com>
2019-09-03 19:13:46 +0700

    Updated Detail LichCongTac

--- resource/views/modules/LichCongTac/Detail.js
+++ resource/views/modules/LichCongTac/Detail.js
@@ -4,7 +4,7 @@ import { View, Text, TouchableOpacity, FlatList } from 'react-native';
 import {
   Container, Header, Left, Button,
   Body, Icon, Title, Content, Form,
-  Tabs, Tab, TabHeading, ScrollableTab, Right
+  Tabs, Tab, TabHeading, ScrollableTab, Right, Subtitle
 } from 'native-base';
 
 import {
@@ -17,6 +17,8 @@ import { NativeBaseStyle } from '../../../assets/styles/NativeBaseStyle';
 import { appGetDataAndNavigate, _readableFormat, convertDateToString } from '../../../common/Utilities';
 import { dataLoading } from '../../../common/Effect';
 import GoBackButton from '../../common/GoBackButton';
+import { GridPanelStyle } from '../../../assets/styles/GridPanelStyle';
+import { SideBarStyle } from '../../../assets/styles/SideBarStyle';
 
 export default class DetailEvent extends Component {
   constructor(props) {
@@ -54,19 +56,34 @@ export default class DetailEvent extends Component {
 
   render() {
     const { data } = this.state;
+
+    let chutriStr = "", chutriArr = [];
+    if (data.TEN_NGUOI_CHUTRI) {
+      chutriArr.push(...data.TEN_NGUOI_CHUTRI.split(", "));
+    }
+    if (data.TEN_VAITRO_CHUTRI) {
+      chutriArr.push(...data.TEN_VAITRO_CHUTRI.split(", "));
+    }
+    if (data.TEN_PHONGBAN_CHUTRI) {
+      chutriArr.push(...data.TEN_PHONGBAN_CHUTRI.split(", "));
+    }
+    if (chutriArr.length > 0) {
+      chutriStr = chutriArr.map(x => `- ${x}`).join("\n");
+    }
     return (
-      <Container>
+      <Container style={{backgroundColor: '#f1f1f1'}}>
         <Header style={{ backgroundColor: Colors.LITE_BLUE }}>
           <Left style={NativeBaseStyle.left}>
             <GoBackButton onPress={() => this.navigateBack()} />
           </Left>
 
           <Body style={NativeBaseStyle.body}>
-            <TouchableOpacity onPress={this.openPicker}>
-              <Title style={NativeBaseStyle.bodyTitle}>
-                CHI TIẾT SỰ KIỆN
-              </Title>
-            </TouchableOpacity>
+            <Title style={NativeBaseStyle.bodyTitle}>
+              CHI TIẾT LỊCH CÔNG TÁC
+            </Title>
+            <Subtitle style={NativeBaseStyle.bodyTitle}>
+              NGÀY {convertDateToString(data.NGAY_CONGTAC)}
+            </Subtitle>
           </Body>
 
           <Right style={NativeBaseStyle.right}>
@@ -74,38 +91,73 @@ export default class DetailEvent extends Component {
           </Right>
         </Header>
 
-        <Content contentContainerStyle={{ flex: 1 }}>
-          {
-            this.state.loading ? dataLoading(this.state.loading) :
-              <View style={{ flex: 1 }}>
-                <View style={{ padding: 10 }}>
-                  <Text style={{ fontWeight: 'bold', fontSize: 17, marginBottom: 15 }}>{data.TIEUDE}</Text>
-                  <Text style={{ marginBottom: 10 }}>
-                    <Text style={{ fontWeight: 'bold', fontSize: 14 }}>Thời gian: </Text>
-                    <Text style={{ fontWeight: 'bold', fontSize: 14, color: Colors.DARK_GRAY }}>
-                      {`${convertDateToString(data.NGAY_CONGTAC)} | ${_readableFormat(data.GIO_CONGTAC)}:${_readableFormat(data.PHUT_CONGTAC)} `}</Text>
-                  </Text>
-                  <Text>
-                    <Text style={{ fontWeight: 'bold', fontSize: 14 }}>Địa điểm: </Text>
-                    <Text style={{ fontWeight: 'bold', fontSize: 14, color: Colors.DARK_GRAY }}>{data.DIADIEM}</Text>
-                  </Text>
-                </View>
-                <View style={{ borderBottomWidth: 1, borderBottomColor: Colors.DARK_GRAY }} />
-
-                <View style={{ padding: 10 }}>
-                  <Text style={{ marginBottom: 10 }}>
-                    <Text style={{ fontWeight: 'bold', fontSize: 14 }}>Chủ trì: </Text>
-                    <Text style={{ fontWeight: 'bold', fontSize: 14, color: Colors.DARK_GRAY }}>{data.TEN_NGUOICHUTRI}</Text>
-                  </Text>
-                </View>
-                <View style={{ borderBottomWidth: 1, borderBottomColor: Colors.DARK_GRAY }} />
-
-                <View style={{ padding: 10 }}>
-                  <Text style={{ fontWeight: 'bold', fontSize: 14, marginBottom: 10 }}>Ghi chú: </Text>
-                  <Text style={{ fontWeight: 'bold', fontSize: 14, color: Colors.DARK_GRAY }}>{data.GHICHU}</Text>
-                </View>
+        <Content contentContainerStyle={{ flex: 1, backgroundColor: '#f1f1f1', paddingVertical: moderateScale(6, 1.2) }} scrollEnabled>
+          <View style={[GridPanelStyle.container, { flexDirection: 'row', justifyContent: 'space-between' }]}>
+            <View style={{ width: "65%" }}>
+              <View style={GridPanelStyle.titleContainer}>
+                <Text style={[GridPanelStyle.listItemTitle, { color: Colors.DANK_GRAY, fontSize: moderateScale(11, 0.9) }]}>Thời gian</Text>
               </View>
+              <View style={{ marginTop: "0.5%" }}>
+                <Text style={{ fontSize: moderateScale(12, 1.2) }}>{`${convertDateToString(data.NGAY_CONGTAC)} | ${_readableFormat(data.GIO_CONGTAC)}:${_readableFormat(data.PHUT_CONGTAC)} `}</Text>
+              </View>
+            </View>
+            <View style={{ width: "35%" }}>
+              <View style={GridPanelStyle.titleContainer}>
+                <Text style={[GridPanelStyle.listItemTitle, { color: Colors.DANK_GRAY, fontSize: moderateScale(11, 0.9) }]}>Địa điểm</Text>
+              </View>
+              <View style={{ marginTop: "0.5%" }}>
+                <Text style={{ fontSize: moderateScale(12, 1.2) }}>{data.DIADIEM}</Text>
+              </View>
+            </View>
+          </View>
+
+          {
+            // <View style={GridPanelStyle.container}>
+            //   <View style={GridPanelStyle.titleContainer}>
+            //     <Text style={[GridPanelStyle.listItemTitle, { color: Colors.DANK_GRAY, fontSize: moderateScale(11, 0.9) }]}>Địa điểm</Text>
+            //   </View>
+            //   <View style={{ marginTop: "0.5%" }}>
+            //     <Text style={{ fontSize: moderateScale(12, 1.2) }}>{data.DIADIEM}</Text>
+            //   </View>
+            // </View>
           }
+
+          <View style={GridPanelStyle.container}>
+            <View style={GridPanelStyle.titleContainer}>
+              <Text style={[GridPanelStyle.listItemTitle, { color: Colors.DANK_GRAY, fontSize: moderateScale(11, 0.9) }]}>Người chủ trì</Text>
+            </View>
+            <View style={{ marginTop: "0.5%" }}>
+              <Text style={{ fontSize: moderateScale(12, 1.2) }}>{chutriStr}</Text>
+            </View>
+          </View>
+
+          <View style={GridPanelStyle.container}>
+            <View style={GridPanelStyle.titleContainer}>
+              <Text style={[GridPanelStyle.listItemTitle, { color: Colors.DANK_GRAY, fontSize: moderateScale(11, 0.9) }]}>Nội dung</Text>
+            </View>
+            <View style={{ marginTop: "0.5%" }}>
+              <Text style={{ fontSize: moderateScale(12, 1.2) }}>{data.NOIDUNG}</Text>
+            </View>
+          </View>
+
+          <View style={GridPanelStyle.container}>
+            <View style={GridPanelStyle.titleContainer}>
+              <Text style={[GridPanelStyle.listItemTitle, { color: Colors.DANK_GRAY, fontSize: moderateScale(11, 0.9) }]}>Chuẩn bị</Text>
+            </View>
+            <View style={{ marginTop: "0.5%" }}>
+              <Text style={{ fontSize: moderateScale(12, 1.2) }}>{data.CHUANBI}</Text>
+            </View>
+          </View>
+
+          <View style={GridPanelStyle.container}>
+            <View style={GridPanelStyle.titleContainer}>
+              <Text style={[GridPanelStyle.listItemTitle, { color: Colors.DANK_GRAY, fontSize: moderateScale(11, 0.9) }]}>Thành phần tham dự</Text>
+            </View>
+            <View style={{ marginTop: "0.5%" }}>
+              <Text style={{ fontSize: moderateScale(12, 1.2) }}>{data.THANHPHAN_THAMDU}</Text>
+            </View>
+          </View>
+
         </Content>
       </Container>
     );
