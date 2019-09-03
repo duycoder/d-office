5ddfb662c5b894614e69b5f4ca6c228ce6c16837
PurpleLover <sepan223@gmail.com>
2019-09-03 19:15:26 +0700

    Cleaned up GridPanel

--- resource/views/common/GridPanel.js
+++ resource/views/common/GridPanel.js
@@ -1,39 +1,26 @@
 /**
- * @description: phần panel dropdown sử dụng animation
- * @author: duynn
- * @since: 05/05/2018
+ * @description: hiển thị chức năng thành dạng lưới
+ * @author: annv
+ * @since: /08/2019
  */
 'use strict';
 import React, { Component } from 'react';
 import {
-  Animated, View, Text, StyleSheet,
-  TouchableOpacity, Image
+  View, Text
 } from 'react-native';
 
-//lib
-import { ListItem } from 'react-native-elements';
 //styles
 import { GridPanelStyle } from '../../assets/styles/GridPanelStyle';
 import { SideBarStyle } from '../../assets/styles/SideBarStyle';
-import { doc_Default, work_Default, account_Default } from '../../assets/styles/SideBarIcons';
-import { verticalScale } from '../../assets/styles/ScaleIndicator';
 
-import SideBar from './SideBar';
-import SideBarIcon from '../../common/Icons';
-import { Colors } from '../../common/SystemConstant';
 import * as util from 'lodash';
 
 export default class GridPanel extends Component {
   constructor(props) {
     super(props);
-    this.icon = require('../../assets/images/arrow.png');
 
     this.state = {
       title: props.title ? util.capitalize(props.title) : "",
-      expanded: props.title === 'CÔNG VIỆC' ? true : false,
-
-      actionCode: props.actionCode,
-      isParent: props.isParent
     };
   }
 
@@ -41,18 +28,7 @@ export default class GridPanel extends Component {
     return (
       <View style={GridPanelStyle.container}>
         <View style={GridPanelStyle.titleContainer}>
-        <Text style={GridPanelStyle.listItemTitle}>{this.state.title}</Text>
-          {
-            // <ListItem
-            //   containerStyle={GridPanelStyle.listItemContainer}
-            //   hideChevron={true}
-            //   title={this.state.title}
-            //   titleStyle={GridPanelStyle.listItemTitle}
-            // // leftIcon={
-            // //   <SideBarIcon actionCode={this.state.actionCode} isParent={this.state.isParent} iconSize={22} iconColor={Colors.GRAY} />
-            // // }
-            // />
-          }
+          <Text style={GridPanelStyle.listItemTitle}>{this.state.title}</Text>
         </View>
         <View style={SideBarStyle.normalBoxContainer}>
           {this.props.children}
